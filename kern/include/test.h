9650aa31dd5801a0a2433b551c082dca91e55037
shannenbravo <shannenbravo@gmail.com>
2017-10-30 13:19:10 -0400

    Update test.h

--- kern/include/test.h
+++ kern/include/test.h
@@ -54,6 +54,7 @@ int threadlisttest(int, char **);
 int threadtest(int, char **);
 int threadtest2(int, char **);
 int threadtest3(int, char **);
+int thread_join_testing(int, char **);
 int semtest(int, char **);
 int locktest(int, char **);
 int cvtest(int, char **);
